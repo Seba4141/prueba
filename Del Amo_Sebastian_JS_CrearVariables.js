2e7ab012333dc6808a65857ac98f0728d6770c54
Sebastian Del Amo <sebastiandelamo41@gmail.com>
2022-09-28 19:09:15 -0300

    complete stretch 1 y 2

--- Del Amo_Sebastian_JS_CrearVariables.js
+++ Del Amo_Sebastian_JS_CrearVariables.js
@@ -1,6 +1,13 @@
-var edad=10
+var edadniño=10
 var alturaniño = 120
-if (alturaniño>=52) {
+if (alturaniño>=52&&edadniño>=8) {
+    console.log("¡Súbete, chico!")  
+}
+else{
+    console.log("Lo siento, chico. Tal vez el próximo año")
+}
+
+if (alturaniño>=52||edadniño>=8) {
     console.log("¡Súbete, chico!")  
 }
 else{
